74c16853f6e66a256ba7864142897e2a0ad3f861
mrlectus <lilbrown3000@gmail.com>
2024-11-23 03:47:52 +0100

    add config

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "standalone",
+  trailingSlash: true,
   serverExternalPackages: ["sharp", "onnxruntime-node"],
 };
 
